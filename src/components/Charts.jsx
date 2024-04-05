68e6e15886da9792407096e13c9b5a7c699fcf0b
Artefilex <baris.tncdmr@gmail.com>
2024-04-05 04:01:36 +0300

    stockApi added for RTK query

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -3,14 +3,15 @@ import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { LineChart } from "@mui/x-charts/LineChart";
+import { data } from "autoprefixer";
 function Charts() {
-  const { debaunce, data: result } = useSelector((state) => state.searchItem);
+  const search = useSelector((state) => state.searchItem.search);
   const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async () => { 
       const { data } = await axios.get(
-        `https://api.polygon.io/v1/indicators/sma/AAPL?timespan=day&adjusted=true&window=50&series_type=close&order=desc&apiKey=${import.meta.env.VITE_EXCHANGE_API}`
+        `https://api.polygon.io/v1/indicators/sma/${search ? search?.debaunce:"AAPL"}?timespan=day&adjusted=true&window=50&series_type=close&order=desc&apiKey=${import.meta.env.VITE_EXCHANGE_API}`
       );
       if (data.results && data.results.values) {
         const formattedData = data.results.values.map((item) => {
@@ -26,24 +27,28 @@ function Charts() {
     };
 
     fetchData();
-    const intervalData = setInterval(fetchData, 60 * 1000);
+    const intervalData = setInterval(fetchData,60 * 60 * 1000);
     return () => clearInterval(intervalData);
-  }, []);
-  const yAxisData = chartData?.map((dataPoint) => dataPoint.y);
-  const xAxisData = chartData?.map((dataPoint) => dataPoint.x);
+  }, [search]);
 
-  // series data preparation
-//   const seriesData = [];
-  console.log(chartData)
+  const yAxisData = chartData?.reduce((acc, curr) => acc + curr.y, 0);
+
+
+
   return (
-    <div>
-      sss
+    <div className="px-4">
       <LineChart
-        xAxis={[{ data: xAxisData }]}
-       
-        series={[{data:chartData}]}
+      
+        yAxis={[{ min: Number( yAxisData / chartData.length) - 5 }]}
+        series={[{ data: chartData?.map((dataPoint) =>Number(dataPoint.y).toFixed(4)), label: search?.debaunce, area: true, showMark: false }]}
+        xAxis={[{ scaleType: 'point', data: chartData?.map((dataPoint) => dataPoint.x).reverse() }]}
         width={500}
         height={300}
+        sx={{ 
+        '.MuiLineElement-root': {
+          display: 'none',
+        },
+      }}
       />
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SearchReducer from "../reducers/SearchSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { StockApi } from "../reducers/StockApi";
 const store = configureStore({
    reducer:{
       searchItem: SearchReducer,
+       [StockApi.reducerPath] : StockApi.reducer
    },
-   
+   middleware: (getDefaultMiddleware) =>
+   getDefaultMiddleware().concat(StockApi.middleware),
 })
 
-
+setupListeners(store.dispatch);
 export default store
